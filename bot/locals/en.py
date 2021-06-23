464ed8118565f68ea230fbcd7264cf3e3b6da211
sanjusingh2021 <86384255+sanjusingh2021@users.noreply.github.com>
2021-06-24 04:06:41 +0530

    Update en.py

--- bot/locals/en.py
+++ bot/locals/en.py
@@ -3,7 +3,7 @@ from bot.locals import Local
 
 LOCAL = Local({
     'WRONG_ROOM' : 'I a\'m not suppose to be here.\nID : <code>{CHAT_ID}</code>',
-    'WELCOME_MESSAGE' : "Hi!\nI'm <b>Deepak Clouds</b>!\nPowered by pyrogram.\nBelongs To: <a href='https://t.me/deepakcloudstorrent</a>",
+    'WELCOME_MESSAGE' : "Hi!\nI'm <b>Deepak Clouds</b>!\nPowered by python program.\nBelongs To: <a href='https://t.me/deepakcloudstorrent</a>",
     'PASS_REQUIRED' : '\n\nUse <code>/{cmd_pass} </code>to enter the password.',
     'LEECH_LIST_MESSAGE_HEADER' : '<b>Leech Status</b>',
     'LEECH_LIST_FORMAT' : 'Name: <code>{name}</code>\nStatus: {status}\nID: <code>{gid}</code>\n\n',
